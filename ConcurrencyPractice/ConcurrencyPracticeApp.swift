4a35d89f3c8f33301a3ef4d015a69e786123ff49
Amish <amish.tufail2002@gmail.com>
2024-01-24 19:10:25 +0500

    Initial Commit

---
+++ ConcurrencyPractice/ConcurrencyPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyPracticeApp.swift
+//  ConcurrencyPractice
+//
+//  Created by Amish Tufail on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
